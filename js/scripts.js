053991ffcad0eaea5fea3abe7f22418ce6ab7aec
videmsky <stuffaccounts@gmail.com>
2013-01-20 08:48:03 -0800

    removed ajax'd content. will template header and footer partials later

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,20 @@
 !function ($) {
   
-  $(function(){
+  function load_ui (){
   	
-  	var wrap = $('div.load-wrap');
-    $('ul.nav>li>a').on('click', function(e) {
-    	var href = $(this).attr('href');
-    	wrap.load(href + ' .container-fluid');
-    	e.preventDefault();
-    });
+  	// var wrap = $('div.load-wrap');
+   //  $('ul.nav>li>a').on('click', function(e) {
+   //  	var href = $(this).attr('href');
+   //  	wrap.load(href + ' .container-fluid');
+   //  	e.preventDefault();
+   //  });
 
-    $('.img-circle').popover({
+    $('img.img-circle').popover({
       placement: 'bottom',
       trigger: 'hover'
     });
- 
 
-  })
+  }
+  $(document).ready(load_ui);
 
 }(window.jQuery)
