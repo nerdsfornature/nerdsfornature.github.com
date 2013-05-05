84dbffbe41bf5a7f8dd532b8e500673c3223fe92
videmsky <stuffaccounts@gmail.com>
2013-05-04 22:50:46 -0700

    added calendar

--- js/scripts.js
+++ js/scripts.js
@@ -33,6 +33,16 @@
       }
     });
 
+    $('.cal-popup').magnificPopup({
+      disableOn: 700,
+      type: 'iframe',
+      mainClass: 'mfp-fade',
+      removalDelay: 160,
+      preloader: false,
+
+      fixedContentPos: false
+    });
+
   }
 
   $(document).ready(load_ui);
